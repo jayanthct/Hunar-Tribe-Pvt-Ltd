e6c66a51b165ae10519650ae37dc7960c333b49c
Jayanth <jayanthct9999@gmail.com>
2025-04-13 16:10:35 +0530

    updated minor details

--- frontend/src/Firebase/FireBaseConfig.js
+++ frontend/src/Firebase/FireBaseConfig.js
@@ -5,13 +5,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAIMVLsFodqHOWMYr-UJjm8DQyBTiJrREQ",
+  authDomain: "hunar-tribe.firebaseapp.com",
+  projectId: "hunar-tribe",
+  storageBucket: "hunar-tribe.firebasestorage.app",
+  messagingSenderId: "1089289253184",
+  appId: "1:1089289253184:web:8f12b4d4e11e5653638aa5",
+  measurementId: "G-M4CYJQZMH4",
 };
 
 // Initialize Firebase
