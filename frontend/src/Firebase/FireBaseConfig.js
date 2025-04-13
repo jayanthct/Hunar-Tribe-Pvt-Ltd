59163922e619550d39c9779829ef1d7860ebe68a
Jayanth <jayanthct9999@gmail.com>
2025-04-13 16:00:59 +0530

    env added and hided in gitignore

--- frontend/src/Firebase/FireBaseConfig.js
+++ frontend/src/Firebase/FireBaseConfig.js
@@ -1,20 +1,17 @@
-// Import the functions you need from the SDKs you need
+// firebase.js or firebaseConfig.js
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAIMVLsFodqHOWMYr-UJjm8DQyBTiJrREQ",
-  authDomain: "hunar-tribe.firebaseapp.com",
-  projectId: "hunar-tribe",
-  storageBucket: "hunar-tribe.firebasestorage.app",
-  messagingSenderId: "1089289253184",
-  appId: "1:1089289253184:web:8f12b4d4e11e5653638aa5",
-  measurementId: "G-M4CYJQZMH4",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
